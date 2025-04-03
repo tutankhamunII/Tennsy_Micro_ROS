5a691eb908ffb9e21078b62e37708685ff8dbbb5
Hesham Elshafey <helshafe@u.rochester.edu>
2025-04-03 16:50:10 -0400

    April 3 2025

--- include/main.hpp
+++ include/main.hpp
@@ -10,6 +10,7 @@
 #include <std_msgs/msg/float32_multi_array.h>
 #include <std_msgs/msg/u_int8_multi_array.h>
 #include <std_msgs/msg/string.h>
+#include <std_msgs/msg/empty.h>
 #if !defined(MICRO_ROS_TRANSPORT_ARDUINO_SERIAL)
 #error This example is only avaliable for Arduino framework with serial transport.
 #endif
@@ -22,15 +23,18 @@ rcl_publisher_t diagnostics_publisher;
 rcl_publisher_t platform_pose_publisher; //May Remove
 //All the subscribers
 rcl_subscription_t actuator_positions_subscriber;
+rcl_subscription_t communication_beat_subscriber;
 //Global messages for publishing and subscriping.
 std_msgs__msg__Float32MultiArray actuator_positions_feedback;
 std_msgs__msg__Float32MultiArray system_currents;
 std_msgs__msg__UInt8MultiArray diagnostics;
 std_msgs__msg__Float32MultiArray system_temperatures;
 std_msgs__msg__Float32MultiArray actuator_positions_command; 
+std_msgs__msg__Empty heart_beat_message;
 //rclc and node initiation.
 rclc_executor_t executor;
 rclc_support_t support;
 rcl_allocator_t allocator;
 rcl_node_t teensy_hexabot_node;
 rcl_timer_t timer;
+
--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,38 @@ void subscription_callback(const void *msgin){
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
+  //Sample the actuator positions and format it into ros2 message
+  //Sample the system currents and format it into ros2 message
+  //Samle the system temperatures and format it into ros2 messge
   if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg2, NULL));
+    RCSOFTCHECK(rcl_publish(&actuator_positions_publisher, &actuator_positions_feedback, NULL));
+    RCSOFTCHECK(rcl_publish(&system_currents_publisher, &system_currents, NULL));
+    RCSOFTCHECK(rcl_publish(&system_temperatures_publisher, &system_temperatures, NULL));
+    RCSOFTCHECK(rcl_publish(&diagnostics_publisher, &diagnostics, NULL));
+    RCSOFTCHECK(rcl_publish(&actuator_positions_publisher, &actuator_positions_feedback, NULL));
   }
 }
 
 void setup() {
   Serial.begin(1000000);
-  set_microros_serial_transports(Serial);
+  
   delay(500);
+  init_microros();
+
+}
+
+void loop() {
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+}
+
+void init_microros(){
+
+  set_microros_serial_transports(Serial);
   allocator = rcl_get_default_allocator();
   //Create the supporter
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-  // create node
+  //Create the teensy node
   RCCHECK(rclc_node_init_default(&teensy_hexabot_node, "teensy_hexabot_node", "", &support));
   //Publisher init for actuator_positions
   RCCHECK(rclc_publisher_init_default(
@@ -61,16 +80,22 @@ void setup() {
       &teensy_hexabot_node,
       ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32MultiArray),
       "/hexabot_driver/actuator_duty_cycle"));
-  // create timer,
-  const unsigned int timer_timeout = 1000;
+  //Subscriber init for communication heart beat
+  RCCHECK(rclc_subscription_init_default(
+      &communication_beat_subscriber,
+      &teensy_hexabot_node,
+      ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Empty),
+      "/hexabot_driver/heart_beat"))
+  //Create timer for publishing the data
+  const unsigned int timer_timeout = 20; //this is time in millisecond, so the timer will fire 50 times in a second
   RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
     RCL_MS_TO_NS(timer_timeout),
     timer_callback));
 
-  // create executor
-  RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+  //Create executor to handle timer and subscription feedbacks.
+  RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
   RCCHECK(rclc_executor_add_timer(&executor, &timer));
   RCCHECK(rclc_executor_add_subscription(
     &executor,
@@ -78,9 +103,25 @@ void setup() {
     &actuator_positions_command,
     &subscription_callback,
     ON_NEW_DATA));
-}
-
-void loop() {
-  delay(100);
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  RCCHECK(rclc_executor_add_subscription(
+    &executor,
+    &communication_beat_subscriber,
+    &heart_beat_message,
+    &subscription_callback,
+    ON_NEW_DATA));
+  //Allocate memory for messages
+  actuator_positions_feedback.data.capacity = 8;
+  actuator_positions_feedback.data.data = (float*) malloc(sizeof(float) * actuator_positions_feedback.data.capacity);
+  actuator_positions_feedback.data.size = 0;
+  system_currents.data.capacity = 9;
+  system_currents.data.data = (float*) malloc(sizeof(float) * system_currents.data.capacity);
+  system_currents.data.size = 0;
+  diagnostics.data.capacity = 10; //change this based on the number of flags we are sending.
+  diagnostics.data.data = (uint8_t*) malloc(sizeof(uint8_t) * diagnostics.data.capacity);
+  diagnostics.data.size = 0;
+  system_temperatures.data.capacity = 2;
+  system_temperatures.data.data = (float*) malloc(sizeof(float) * system_temperatures.data.capacity);
+  actuator_positions_command.data.capacity = 8;
+  actuator_positions_command.data.data = (float*) malloc(sizeof(float) * actuator_positions_command.data.capacity);
+  actuator_positions_command.data.size = 8;  
 }
