f136a58ae85e09b37184047c5cee7aef86670fe1
Omar Khalil <ozakaria@u.rochester.edu>
2025-04-05 20:43:44 -0400

    teensy pins

--- include/main.hpp
+++ include/main.hpp
@@ -38,3 +38,73 @@ rcl_allocator_t allocator;
 rcl_node_t teensy_hexabot_node;
 rcl_timer_t timer;
 
+//Naming scheme follows meaningful name + "pin" to indicate a constant for 
+//pin assignment to the Teensy. Anything else is a variable used in dynamically in the code
+//////// WE NEED TO ADD ALL MICROROS ASSIGNMENTS HERE INCLUDING THE MACROS.
+//LEDs pin assignmnets (Digital Output)
+constexpr int power_LED_blue_pin = 29;
+constexpr int power_LED_green_pin = 30;
+constexpr int power_LED_red_pin = 31;
+constexpr int communication_LED_blue_pin = 32;
+constexpr int communication_LED_green_pin = 33;
+constexpr int communication_LED_red_pin = 34;
+//Diagnostic pin assignments (Mixed)
+constexpr int switch_control_pin = 15;
+constexpr int voltage_translator_control_pin = 28;
+constexpr int estop_signal_pin = 35;
+constexpr int switch_status_pin = 36;
+constexpr int main_power_feedback_pin = 37;
+constexpr int total_current_feedback_pin = 38;
+constexpr int temperature_pin = 26;
+//Servo pin assignments (Mixed)
+constexpr int servo_position_control_pin = 0;
+constexpr int servo_power_control_pin = 24;
+constexpr int servo_current_feedback_pin = 39;
+//Actuator position control pin assignmnets (PWM)
+constexpr int actuator_1_position_control_pin = 6;
+constexpr int actuator_2_position_control_pin = 5;
+constexpr int actuator_3_position_control_pin = 4;
+constexpr int actuator_4_position_control_pin = 3;
+constexpr int actuator_5_position_control_pin = 2;
+constexpr int actuator_6_position_control_pin = 1;
+//Actuator power control pin assignmnets (Digital Output)
+constexpr int actuator_3_power_control_pin = 7;
+constexpr int actuator_2_power_control_pin = 8;
+constexpr int actuator_1_power_control_pin = 9;
+constexpr int actuator_4_power_control_pin = 10;
+constexpr int actuator_5_power_control_pin = 11;
+constexpr int actuator_6_power_control_pin = 12;
+//Actuator position feedback pin assignmnets (Analog Input)
+constexpr int actuator_1_position_feedback_pin = 23;
+constexpr int actuator_2_position_feedback_pin = 22;
+constexpr int actuator_3_position_feedback_pin = 21;
+constexpr int actuator_4_position_feedback_pin = 20;
+constexpr int actuator_5_position_feedback_pin = 19;
+constexpr int actuator_6_position_feedback_pin = 18;
+//Actuator current feedback pin assignmnets (Analog Input)
+constexpr int actuator_1_current_feedback_pin = 17;
+constexpr int actuator_2_current_feedback_pin = 16;
+constexpr int actuator_3_current_feedback_pin = 15;
+constexpr int actuator_4_current_feedback_pin = 14;
+constexpr int actuator_5_current_feedback_pin = 41;
+constexpr int actuator_6_current_feedback_pin = 40;
+
+//Globals for position feedback in millimeters.
+float actuator_1_position_feedback = 0;
+float actuator_2_position_feedback = 0;
+float actuator_3_position_feedback = 0;
+float actuator_4_position_feedback = 0;
+float actuator_5_position_feedback = 0;
+float actuator_6_position_feedback = 0;
+float servo_position_feedback = 0;
+//Globals for duty cycles (in 0 to 32... analog range) to the actuators
+int actuator_1_duty_cycle = 0;
+int actuator_2_duty_cycle = 0;
+int actuator_3_duty_cycle = 0;
+int actuator_4_duty_cycle = 0;
+int actuator_5_duty_cycle = 0;
+int actuator_6_duty_cycle = 0;
+int servo_duty_cycle = 0;
+void emergency_ISR();
+void emergency_sequance();
+void recover_from_emergency();
